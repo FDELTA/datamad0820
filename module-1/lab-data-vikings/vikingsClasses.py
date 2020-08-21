f5ce36b1cb4a910568b6f240bbaccf13588feb16
Delta <f.delgadoteran@gmail.com>
2020-08-21 17:01:35 +0200

    Ejercicio Vikings

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,12 +3,36 @@
 
 
 class Soldier:
+    def __init__(self, health=100, strength=20):
+        self.health = health
+        self.strength = strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self,damage):
+        self.health -= damage
+
     pass
 
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    def __init__(self, name, health=100, strength=20):
+        super().__init__(health, strength)
+        self.name = name
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.health -= damage
+        extra_health = self.health
+        if extra_health > 0:
+            return (f"{self.name} has received {damage} points of damage")
+        else:
+            return (f"{self.name} has died in act of combat")
+    def battleCry(self):
+        return 'Odin Owns You All!'
+
+
     pass
 
 # Saxon
